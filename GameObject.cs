94684430a18c66db6f7fce65a07bb248034d0054
Diego4458 <40145391+Diego4458@users.noreply.github.com>
2020-03-03 22:13:43 -0300

    Correção ImOutOfBound()
    Fixação Do Y Perante O tamanho da tela

--- GameObject.cs
+++ GameObject.cs
@@ -43,22 +43,24 @@ namespace FreeScript
             {
                 Position.X = 0;
             }
-            if (Position.Y < 0)
+            else if (Position.Y < 0)
             {
                 Position.Y = 0;
             }
-            if (Position.X+Size.X >= 500 )
+            else if (Position.Y + Size.X >= 500 && Position.X + Size.X >= 500)
+            {
+                Position.X = 500 - Size.X;
+                Position.Y = 500 - Size.Y;
+            }
+            else if (Position.X + Size.X >= 500)
             {
                 Position.X = 500 - Size.X;
             }
-            else if (Position.Y >= 500)
+            else if (Position.Y + Size.Y >= 500)
             {
-                Position.Y = 500;
+                Position.Y = 500 - Size.Y;
             }
-            else
-            {
-                this.UpdateLocation();
-            }
+            this.UpdateLocation();
         }
 
         public bool ofCourse()
